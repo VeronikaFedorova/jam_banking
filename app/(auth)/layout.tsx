8df6b1b26a958ea35ed0501939d085d67e5f5627
Veronika Fedorova <fedorovavv03@gmail.com>
2024-08-19 18:51:31 +0300

    Add a HeaderBox component

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,6 +1,3 @@
-import type { Metadata } from 'next';
-import { Inter, IBM_Plex_Serif } from 'next/font/google';
-
 export default function RootLayout({
   children,
 }: Readonly<{
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,23 @@
-const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+import HeaderBox from '@/components/HeaderBox';
+import TotalBalanceBox from '@/components/TotalBalanceBox';
 
-export default Home
+const Home = () => {
+  const loggedIn = { firstName: 'Veronika' };
+  return (
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+          <HeaderBox
+            type='greeting'
+            title='Welcome'
+            user={loggedIn ? loggedIn.firstName : 'Guest'}
+            subtext='Access and manage your account and transaction efficiently.'
+          />
+          <TotalBalanceBox accounts={[]} totalBanks={1} totalCurrentBalance={1250.35} />
+        </header>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
