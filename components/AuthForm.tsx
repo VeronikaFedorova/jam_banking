8683c10ddc5a86094a590f2f302c4495fd750e21
Veronika Fedorova <fedorovavv03@gmail.com>
2024-09-02 14:48:54 +0300

    Add appwrite functions

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -19,8 +19,11 @@ import { Input } from '@/components/ui/input';
 import CustomInput from './CustomInput';
 import { authformSchema } from '@/lib/utils';
 import { Loader2 } from 'lucide-react';
+import { useRouter } from 'next/router';
+import { signIn, signUp } from '@/lib/actions/user.actions';
 
 const AuthForm = ({ type }: { type: string }) => {
+  const router = useRouter();
   const [user, setUser] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
 
@@ -37,13 +40,16 @@ const AuthForm = ({ type }: { type: string }) => {
   const onSubmit = async (data: z.infer<typeof formSchema>) => {
     setIsLoading(true);
     try {
-      //Sign up with Appwrite & create plaid token
       if (type === 'sign-up') {
-        const userData = {
-          firstName: data.firstName,
-        }
+        const newUser = await signUp(data);
+        setUser(newUser);
       }
       if (type === 'sign-in') {
+        const response = await signIn({
+          email: data.email,
+          password: data.password,
+        });
+        if(response) router.push('/');
       }
     } catch (error) {
       console.log(error);
@@ -104,6 +110,12 @@ const AuthForm = ({ type }: { type: string }) => {
                     name='address1'
                     label='Address'
                     placeholder='Enter your specific address'
+                  />
+                  <CustomInput
+                    control={form.control}
+                    name='city'
+                    label='City'
+                    placeholder='Enter your specific city'
                   />
                   <div className='flex gap-4'>
                     <CustomInput
